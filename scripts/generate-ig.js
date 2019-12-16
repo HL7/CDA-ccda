992a2318d68a8dd32b1267b9e4e9204d34b5ffe4
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2019-12-15 21:14:23 -0800

    Updates to include invariants and set an order of the constraints so that slices don't come out (theoretically in different orders every time)

--- scripts/generate-ig.js
+++ scripts/generate-ig.js
@@ -1,49 +1,30 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs = require("fs");
-const path = require("path");
 const vkbeautify = require("vkbeautify");
 const fhir_1 = require("fhir");
 const fhir = new fhir_1.Fhir();
-const strucDefDir = 'resources\\structuredefinition';
-const strucDefFiles = fs.readdirSync('resources\\structuredefinition');
-const valueSetDir = 'resources\\valueset';
-const valueSetFiles = fs.readdirSync('resources\\valueset');
-const strucDefs = strucDefFiles.map((file) => {
-    const content = fs.readFileSync(path.join(strucDefDir, file));
-    const resource = fhir.xmlToObj(content);
-    return resource;
+const strucDefFiles = fs.readdirSync('input/resources/structuredefinition');
+const valueSetFiles = fs.readdirSync('input/resources/valueset');
+const igContent = fs.readFileSync('input/ccda.xml').toString();
+const ig = fhir.xmlToObj(igContent);
+const valueSetResources = valueSetFiles.map(valueSetFile => {
+    const id = valueSetFile.substring(valueSetFile.lastIndexOf('/'), valueSetFile.indexOf('.xml'));
+    return {
+        reference: {
+            reference: `ValueSet/${id}`
+        }
+    };
 });
-const valueSets = valueSetFiles.map((file) => {
-    const content = fs.readFileSync(path.join(valueSetDir, file));
-    const resource = fhir.xmlToObj(content);
-    return resource;
+const strucDefResources = strucDefFiles.map(strucDefFile => {
+    const id = strucDefFile.substring(strucDefFile.lastIndexOf('/'), strucDefFile.indexOf('.xml'));
+    return {
+        reference: {
+            reference: `StructureDefinition/${id}`
+        }
+    };
 });
-const ig = {
-    resourceType: 'ImplementationGuide',
-    id: 'ccda',
-    url: 'http://hl7.org/cda/us/ccda',
-    name: 'ccda',
-    title: 'Consolidated CDA Templates for Clinical Notes (US Realm) DSTU R2.1',
-    packageId: 'hl7.cda.us.ccda',
-    version: '2.1.0',
-    status: 'active',
-    experimental: false,
-    publisher: 'Health Level 7',
-    //contact: [],
-    fhirVersion: ['3.0.1'],
-    //global: []                    // Should be profiles like US-Realm-Header, US-Realm-Patient, etc.
-    definition: {
-        resource: strucDefs.concat(valueSets).map((resource) => {
-            return {
-                reference: {
-                    reference: resource.resourceType + '/' + resource.id
-                },
-                example: resource.resourceType !== 'StructureDefinition' && resource.resourceType !== 'ValueSet'
-            };
-        })
-    }
-};
+ig.definition.resource = strucDefResources.concat(valueSetResources);
 const igXml = vkbeautify.xml(fhir.objToXml(ig));
-fs.writeFileSync('resources/implementationguide/ccda.xml', igXml);
+fs.writeFileSync('input/ccda.xml', igXml);
 //# sourceMappingURL=generate-ig.js.map
