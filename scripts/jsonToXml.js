0e7a5d3aa9717233f738f66fbfce664287f32d53
Benjamin Flessner <b.d.flessner@gmail.com>
2023-10-24 16:15:48 -0500

    Update jsonToXml to keep descriptions in sync

--- scripts/jsonToXml.js
+++ scripts/jsonToXml.js
@@ -4,70 +4,88 @@ const FhirVersions = require('fhir').Versions;
 const fs = require('fs');
 const path = require('path');
 const xmlFormat = require('xml-formatter');
+const xml2js = require('xml2js');
 
+(async function () {
+  // Need to load R5 for this to work....for now
+  // Get the data
+  const newValueSets = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'r5.valuesets.json')).toString());
+  const newTypes = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'r5.profiles-types.json')).toString());
+  const newResources = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'r5.profiles-resources.json')).toString());
+  // Create a parser and parse it using the parser
+  var parser = new ParseConformance(false, FhirVersions.R5);           // don't load pre-parsed data
+  parser.parseBundle(newValueSets);
+  parser.parseBundle(newTypes);
+  parser.parseBundle(newResources);
 
-// Need to load R5 for this to work....for now
-// Get the data
-const newValueSets = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'r5.valuesets.json')).toString());
-const newTypes = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'r5.profiles-types.json')).toString());
-const newResources = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'r5.profiles-resources.json')).toString());
-// Create a parser and parse it using the parser
-var parser = new ParseConformance(false, FhirVersions.R5);           // don't load pre-parsed data
-parser.parseBundle(newValueSets);
-parser.parseBundle(newTypes);
-parser.parseBundle(newResources);
+  /**
+   * Convert Sushi's JSON output to XML and move into the input directory
+   */
 
-/**
- * Convert Sushi's JSON output to XML and move into the input directory
- */
+  const fhir = new Fhir(parser);
 
-const fhir = new Fhir(parser);
+  const fshDirectory = 'fsh-tank/fsh-generated/resources';
+  const xmlDirectory = 'input/resources';
 
-const fshDirectory = 'fsh-tank/fsh-generated/resources';
-const xmlDirectory = 'input/resources';
+  const ig = fs.readFileSync('input/hl7.cda.us.ccda.xml');
+  const xmlParser = new xml2js.Parser({ explicitArray: false });
 
-// Read the files in the directory
-fs.readdir(fshDirectory, (err, files) => {
-  if (err) {
-    console.error('Error reading directory:', err);
-    return;
-  }
+  const ccdaIg = await xmlParser.parseStringPromise(ig);
 
-  let tempCnt = 0;
-  // Iterate through each file
-  files.forEach(async (file) => {
-    if (path.extname(file) === '.json') {
-      const filePath = path.join(fshDirectory, file);
+  const resources = ccdaIg.ImplementationGuide.definition.resource;
 
-      const data = fs.readFileSync(filePath, 'utf8');
-      const json = JSON.parse(data);
-
-      // Add the logical-target extension
-      // if (!json.extension) json.extension = [];
-      // json.extension.push({
-      //   url: 'http://hl7.org/fhir/tools/StructureDefinition/logical-target',
-      //   _valueBoolean: {
-      //     extension: [{
-      //       url: 'http://hl7.org/fhir/StructureDefinition/data-absent-reason',
-      //       valueCode: 'not-applicable'
-      //     }]
-      //   }
-      // })
-
-      const xml = fhir.jsonToXml(JSON.stringify(json));
-      if (!xml) {
-        console.error(`Failed to convert ${filePath}`);
-        return;
-      }
-      const pretty = xmlFormat(xml, {});
-
-      // console.log(pretty);
-      // return;
-
-
-      const newPath = path.join(xmlDirectory, file.replace('.json', '.xml'));
-      fs.writeFileSync(newPath, pretty, 'utf8');
+  // Read the files in the directory
+  fs.readdir(fshDirectory, (err, files) => {
+    if (err) {
+      console.error('Error reading directory:', err);
+      return;
     }
-  });
 
-});
+    let tempCnt = 0;
+    // Iterate through each file
+    files.forEach(async (file) => {
+      if (path.extname(file) === '.json') {
+        const filePath = path.join(fshDirectory, file);
+
+        const data = fs.readFileSync(filePath, 'utf8');
+        const json = JSON.parse(data);
+
+        // Make sure the description in the IG matches the description from the SD
+        const sdId = json.id;
+        const igResource = resources.find(r => r.reference.reference.$.value == `StructureDefinition/${sdId}`);
+        if (!igResource) {
+          console.warn(`SD ${sdId} is not in the IG!`);
+        } else {
+          igResource.description.$.value = json.description;
+        }
+
+        const xml = fhir.jsonToXml(JSON.stringify(json));
+        if (!xml) {
+          console.error(`Failed to convert ${filePath}`);
+          return;
+        }
+        const pretty = xmlFormat(xml, {});
+
+        const newPath = path.join(xmlDirectory, file.replace('.json', '.xml'));
+        fs.writeFileSync(newPath, pretty, 'utf8');
+      }
+    });
+
+
+    // Write the updated IG resource
+    const builder = new xml2js.Builder({
+      renderOpts: {
+        pretty: true,
+        indent: '    ',
+        newline: '\n'
+      },
+      xmldec: {
+        version: '1.0',
+        encoding: 'UTF-8',
+      }
+    });
+    const newXml = builder.buildObject(ccdaIg);
+    fs.writeFileSync('input/hl7.cda.us.ccda.xml', newXml);
+
+  });
+})();
